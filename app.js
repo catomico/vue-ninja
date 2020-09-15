531bc1513950d4cb30535118fe12ff4e377c0ce8
user86 <user86marc@gmail.com>
2020-09-15 07:42:14 +0200

    video 3

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let app = new Vue({
   el: '#app',
   data: {
-    intro: 'Hello There!'
+    intro: 'Hello There!',
+    name: 'General Kenobi',
+    pic: imgUrl('https://i2.sndcdn.com/avatars-Orvd7eN1YbCylbnx-eqXaEg-t500x500.jpg')
   },
-  method: { greet: '' }
+  methods: { 
+    greet: function(time) {
+      return 'Good' + ' ' + time + '!' + ' ' + this.name;
+    } 
+  }
 });
